33e2955a7ad684537976cab6b1fe23405be822b5
PauloEd-hub <pauloedu.cavalcante@gmail.com>
2023-01-23 13:32:49 -0300

    Criação das classes de entidades.

---
+++ src/main/java/com/paulocavalcante/vendas/vendas/domain/entity/Cliente.java
@@ -0,0 +1,24 @@
+package com.paulocavalcante.vendas.vendas.domain.entity;
+
+public class Cliente {
+
+    private Integer id;
+    private String nome;
+
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+}
