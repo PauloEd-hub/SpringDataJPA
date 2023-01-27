de816a80f498df795ac0660d0fd86ae2b38d9e36
PauloEd-hub <pauloedu.cavalcante@gmail.com>
2023-01-27 17:05:51 -0300

    Exemplificando trabalho com JDBC e criando CRUD simples.

--- src/main/java/com/paulocavalcante/vendas/vendas/domain/repositorio/Clientes.java
+++ src/main/java/com/paulocavalcante/vendas/vendas/domain/repositorio/Clientes.java
@@ -17,6 +17,12 @@ public class Clientes {
     private static String INSERT = "insert into cliente (nome) values (?)";
     private static String SELECT_ALL = "SELECT * FROM CLIENTE";
 
+    private static String UPDATE = "update cliente set nome = ? where id = ? ";
+
+    private static String DELETE = "delete from cliente where id = ? ";
+
+//    private static String SELECT_POR_NOME =
+
     @Autowired
     private JdbcTemplate jdbcTemplate;
     public Cliente salvar(Cliente cliente) {
@@ -25,14 +31,42 @@ public class Clientes {
 
     }
 
+    public Cliente atualizar(Cliente cliente) {
+        jdbcTemplate.update(UPDATE, new Object[]{
+                cliente.getNome(),
+                cliente.getId()});
+
+        return cliente;
+
+    }
+
+    public void deletar(Cliente cliente) {
+        deletar(cliente.getId());
+    }
+
+    public void deletar(Integer id) {
+        jdbcTemplate.update(DELETE, new Object[]{id});
+
+    }
+
+    public List<Cliente> buscarPorNome(String nome) {
+        return jdbcTemplate.query(
+                SELECT_ALL.concat(" where nome like ? "),
+                new Object[]{"%" + nome + "%"},
+                obterClienteMapper());
+
+    }
+
     public List<Cliente> obterTodos() {
-        return jdbcTemplate.query(SELECT_ALL, new RowMapper<Cliente>() {
-            @Override
-            public Cliente mapRow(ResultSet rs, int rowNum) throws SQLException {
-                Integer id = rs.getInt("id");
-                String nome = rs.getString("nome");
-                return new Cliente(id, nome);
-            }
-        });
+        return jdbcTemplate.query(SELECT_ALL, obterClienteMapper());
+
+    }
+
+    private RowMapper<Cliente> obterClienteMapper() {
+        return (rs, rowNum) -> {
+            Integer id = rs.getInt("id");
+            String nome = rs.getString("nome");
+            return new Cliente(id, nome);
+        };
     }
 }
