bb033c9e4af750740c35670f1b32cc8db3292251
PauloEd-hub <pauloedu.cavalcante@gmail.com>
2023-02-15 21:34:59 -0300

    Melhorando controller de clientes.

--- src/main/java/com/paulocavalcante/vendas/vendas/VendasApplication.java
+++ src/main/java/com/paulocavalcante/vendas/vendas/VendasApplication.java
@@ -1,10 +1,24 @@
 package com.paulocavalcante.vendas.vendas;
 
+import com.paulocavalcante.vendas.vendas.domain.entity.Cliente;
+import com.paulocavalcante.vendas.vendas.domain.repository.Clientes;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class VendasApplication {
+
+
+	@Bean
+	public CommandLineRunner commandLineRunner(@Autowired Clientes clientes) {
+		return args -> {
+			Cliente c = new Cliente(null, "Fulano");
+			clientes.save(c);
+		};
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(VendasApplication.class, args);
 	}
--- src/main/java/com/paulocavalcante/vendas/vendas/rest/controller/ClienteController.java
+++ src/main/java/com/paulocavalcante/vendas/vendas/rest/controller/ClienteController.java
@@ -1,21 +1,34 @@
 package com.paulocavalcante.vendas.vendas.rest.controller;
 
 
+import com.paulocavalcante.vendas.vendas.domain.entity.Cliente;
+import com.paulocavalcante.vendas.vendas.domain.repository.Clientes;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Optional;
 
 @Controller
 @RequestMapping("/api/clientes")
 public class ClienteController {
 
+    @Autowired
+    private Clientes clientes;
 
-    @RequestMapping(value = "/hello/{nome}", method = RequestMethod.GET)
+
+    @GetMapping("/{id}")
     @ResponseBody
-    public String helloCliente(@PathVariable("nome") String nomeCliente) {
-        return String.format("Hello %s ", nomeCliente);
+    public ResponseEntity<Cliente> getClienteById(@PathVariable Integer id) {
+        Optional<Cliente> cliente = clientes.findById(id);
 
+        if(cliente.isPresent()) {
+            return  ResponseEntity.ok(cliente.get());
+        }
+        return ResponseEntity.notFound().build();
     }
 }
