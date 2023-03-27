090923321ad510d93255859e20c8d0744f33bb18
PauloEd-hub <pauloedu.cavalcante@gmail.com>
2023-03-27 10:49:14 -0300

    ExceptionHandlers para tratar erros na API.

---
+++ src/main/java/com/paulocavalcante/vendas/vendas/rest/ApiErrors.java
@@ -0,0 +1,16 @@
+package com.paulocavalcante.vendas.vendas.rest;
+
+import lombok.Getter;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class ApiErrors {
+
+    @Getter
+    private List<String> errors;
+
+    public ApiErrors(String mensagemErro) {
+        this.errors = Arrays.asList(mensagemErro);
+    }
+}
---
+++ src/main/java/com/paulocavalcante/vendas/vendas/rest/controller/ApplicationControllerAdvice.java
@@ -0,0 +1,21 @@
+package com.paulocavalcante.vendas.vendas.rest.controller;
+
+
+import com.paulocavalcante.vendas.vendas.exception.RegraNegocioException;
+import com.paulocavalcante.vendas.vendas.rest.ApiErrors;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class ApplicationControllerAdvice {
+
+
+    @ExceptionHandler(RegraNegocioException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ApiErrors handleRegraNegocioException(RegraNegocioException ex) {
+        String mensagemErro = ex.getMessage();
+        return new ApiErrors(mensagemErro);
+    }
+}
